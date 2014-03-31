cde63f9bd5264ae62e0a7b1d231f3e1771cf9b4d
Mike Purvis <mpurvis@clearpathrobotics.com>
2014-03-31 12:11:40 -0400

    Rebase from 'release/indigo/interactive_marker_twist_server'

--- src/marker_server.cpp
+++ src/marker_server.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2011, Willow Garage, Inc.
+ *           (c) 2013, Mike Purvis
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -27,14 +28,18 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <ros/ros.h>
 #include <interactive_markers/interactive_marker_server.h>
-#include <string.h>
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/Pose.h>
+#include <ros/ros.h>
 #include <tf/tf.h>
 
-using namespace visualization_msgs;
+#include <algorithm>
+#include <string>
+
+using visualization_msgs::InteractiveMarker;
+using visualization_msgs::InteractiveMarkerControl;
+using visualization_msgs::InteractiveMarkerFeedback;
 
 class MarkerServer
 {
@@ -46,62 +51,75 @@ class MarkerServer
 
       nh.param<std::string>("link_name", link_name, "/base_link");
       nh.param<std::string>("robot_name", robot_name, "robot");
+
       nh.param<double>("linear_scale", linear_drive_scale, 1.0);
       nh.param<double>("angular_scale", angular_drive_scale, 2.2);
       nh.param<double>("marker_size_scale", marker_size_scale, 1.0);
 
-      vel_pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
+      nh.param<double>("max_positive_linear_velocity", max_positive_linear_velocity, 1.0);
+      nh.param<double>("max_negative_linear_velocity", max_negative_linear_velocity, -1.0);
+      nh.param<double>("max_angular_velocity", max_angular_velocity, 2.2);
+
+      vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 1);
       createInteractiveMarkers();
 
       ROS_INFO("[twist_marker_server] Initialized.");
     }
 
     void processFeedback(
-        const InteractiveMarkerFeedbackConstPtr &feedback );
-  
+        const InteractiveMarkerFeedback::ConstPtr &feedback);
+
   private:
     void createInteractiveMarkers();
-  
+
     ros::NodeHandle nh;
     ros::Publisher vel_pub;
     interactive_markers::InteractiveMarkerServer server;
-    
+
     double linear_drive_scale;
     double angular_drive_scale;
-
+    double max_positive_linear_velocity;
+    double max_negative_linear_velocity;
+    double max_angular_velocity;
     double marker_size_scale;
-    
+
     std::string link_name;
     std::string robot_name;
 };
 
 void MarkerServer::processFeedback(
-    const InteractiveMarkerFeedbackConstPtr &feedback )
+    const InteractiveMarkerFeedback::ConstPtr &feedback )
 {
   // Handle angular change (yaw is the only direction in which you can rotate)
   double yaw = tf::getYaw(feedback->pose.orientation);
-  
+
   geometry_msgs::Twist vel;
   vel.angular.z = angular_drive_scale * yaw;
   vel.linear.x = linear_drive_scale * feedback->pose.position.x;
 
-  vel_pub.publish(vel);    
-  
+  // Enforce parameterized speed limits
+  vel.linear.x = std::min(vel.linear.x, max_positive_linear_velocity);
+  vel.linear.x = std::max(vel.linear.x, max_negative_linear_velocity);
+  vel.angular.z = std::min(vel.angular.z, max_angular_velocity);
+  vel.angular.z = std::max(vel.angular.z, -max_angular_velocity);
+
+  vel_pub.publish(vel);
+
   // Make the marker snap back to robot
   server.setPose(robot_name + "_twist_marker", geometry_msgs::Pose());
-  
+
   server.applyChanges();
 }
 
 void MarkerServer::createInteractiveMarkers()
-{ 
+{
   // create an interactive marker for our server
   InteractiveMarker int_marker;
   int_marker.header.frame_id = link_name;
   int_marker.name = robot_name + "_twist_marker";
   int_marker.description = "twist controller for " + robot_name;
   int_marker.scale = marker_size_scale;
-  
+
   InteractiveMarkerControl control;
 
   control.orientation_mode = InteractiveMarkerControl::FIXED;
@@ -118,9 +136,9 @@ void MarkerServer::createInteractiveMarkers()
   control.orientation.y = 1;
   control.orientation.z = 0;
   control.name = "rotate_z";
-  
+
   control.interaction_mode = InteractiveMarkerControl::MOVE_ROTATE;
-  //control.interaction_mode = InteractiveMarkerControl::ROTATE_AXIS;
+  // control.interaction_mode = InteractiveMarkerControl::ROTATE_AXIS;
   int_marker.controls.push_back(control);
 
   // Commented out for non-holonomic robot. If holonomic, can move in y.
@@ -131,9 +149,9 @@ void MarkerServer::createInteractiveMarkers()
   control.name = "move_y";
   control.interaction_mode = InteractiveMarkerControl::MOVE_AXIS;
   int_marker.controls.push_back(control);*/
-  
-  server.insert(int_marker, boost::bind( &MarkerServer::processFeedback, this, _1 ));
-  
+
+  server.insert(int_marker, boost::bind(&MarkerServer::processFeedback, this, _1));
+
   server.applyChanges();
 }
 
@@ -142,6 +160,6 @@ int main(int argc, char** argv)
 {
   ros::init(argc, argv, "marker_server");
   MarkerServer server;
-  
+
   ros::spin();
 }
